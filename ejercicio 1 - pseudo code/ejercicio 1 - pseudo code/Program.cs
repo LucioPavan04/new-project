c39a6b3e3be97915c1456751bebbb5dcb9648e08
LucioPavan04 <luciomartin02@gmail.com>
2024-04-09 17:11:47 -0300

    primer commit
    primer commit donde pegamos los ejercicios de la clase pasada

---
+++ ejercicio 1 - pseudo code/ejercicio 1 - pseudo code/Program.cs
@@ -0,0 +1,16 @@
+﻿//suma entre dos números
+
+float numero1, numero2, resultado;
+Console.WriteLine("ingrese un numero");
+numero1 = 
+
+    float.Parse(s: Console.ReadLine());
+
+Console.WriteLine("ingrese un numero");
+numero2 =
+    float.Parse(s: Console.ReadLine());
+
+resultado = numero1 + numero2;
+
+Console.WriteLine("El resultado es " + resultado);
+    Console.ReadLine();
